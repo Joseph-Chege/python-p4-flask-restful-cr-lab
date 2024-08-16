3f4b1bb73b0010ef55a9f6b03dba9ef15fea3214
Joseph-Chege <mjchege23@gmail.com>
2024-08-16 15:32:09 +0300

    Completed the REST create and retrieve lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,53 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(plants, 200)
+
+        return response
+
+api.add_resource(Plants, '/plants')
+    
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return jsonify({'error': 'Plant not found'}, 404)
         
+        response = make_response(plant.to_dict(), 200)
+
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+class CreatePlant(Resource):
+    def post(self):
+        data = request.get_json()
+
+        if 'name' not in data or 'image' not in data or 'price' not in data:
+            return jsonify({'error': 'Missing required fields'}), 400
+        
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(new_plant.to_dict(), 201)
+        response.headers['Location'] = f'/plants/{new_plant.id}'
+
+        return response
+    
+api.add_resource(CreatePlant, '/plants')
+         
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
